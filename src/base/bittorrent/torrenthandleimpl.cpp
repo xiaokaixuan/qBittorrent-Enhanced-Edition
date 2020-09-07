0634a6a6523a1506d4fd29f48994b6ee758246d9
c0re100 <corehusky@gmail.com>
2020-09-07 22:00:55 +0800

    Save torrent resume data when enabling IP Filter

--- src/base/bittorrent/torrenthandleimpl.cpp
+++ src/base/bittorrent/torrenthandleimpl.cpp
@@ -2144,4 +2144,5 @@ QVector<qreal> TorrentHandleImpl::availableFileFractions() const
 void TorrentHandleImpl::enableIPFilter() const
 {
     m_nativeHandle.set_flags(lt::torrent_flags::apply_ip_filter);
+    m_nativeHandle.save_resume_data();
 }
